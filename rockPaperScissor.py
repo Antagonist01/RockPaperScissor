410fc2d2e486f70ed07d403a4a4de4af17107910
Shivam <150827282+Antagonist01@users.noreply.github.com>
2025-02-07 12:22:53 +0530

    Update rockPaperScissor.py
    removed the unnecessary comment leaving the main code intact.

--- rockPaperScissor.py
+++ rockPaperScissor.py
@@ -26,25 +26,6 @@ scissors = '''
 ---.__(___)
 '''
 
-'''user_choice= int (input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors"))
-if user_choice == 0:
-    print(rock)
-elif user_choice == 1:
-    print(paper)
-elif user_choice == 2:
-    print(scissors)
-else:
-    print("Invalid Choice")
-
-all_choices = [rock, paper, scissors]
-random_num = random.randint(a=0, b=2)
-random_move = all_choices.pop(random_num)
-print (random_move)
-
-if user_choice == random_move:
-    print("Its a draw")
-elif user_choice == 1 and random_move =='''
-
 game_images = [rock, paper, scissors]
 # going to let the user choose a number
 user_choice =  int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
